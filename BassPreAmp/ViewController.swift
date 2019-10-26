7f377bdfc3c5e73f3f6fcb21f8608854fcdf1c33
Jair-Rohm <jairrohm@gmail.com>
2019-10-25 23:55:08 -0400

    Changed the icon and fixed the dist code so that it sounds smoother. I also took away the constraints, changed the fonts and added a background.

--- BassPreAmp/ViewController.swift
+++ BassPreAmp/ViewController.swift
@@ -28,36 +28,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var resSlider: UISlider!
     @IBOutlet weak var cfSlider: UISlider!
     
-    var player = AKPlayer()
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var callousLable: UILabel!
+    @IBOutlet weak var edgeLabel: UILabel!
+    @IBOutlet weak var strideLabel: UILabel!
+    @IBOutlet weak var rumpLabel: UILabel!
+    
+    
     var loFilter: AKMoogLadder?
     var distMixer: AKDryWetMixer?
     var callousness: AKDistortion?
-    var filterBand2: AKEqualizerFilter?
+    var filterBand2: AKEqualizerFilter!
     var filterBand3: AKEqualizerFilter?
     var filterBand7: AKEqualizerFilter?
     var booster: AKBooster?
     var silence: AKBooster?
-    var input: AKMicrophone?
+    let input = AKMicrophone()
     
     
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        // let tracker = AKFrequencyTracker.init(player, hopSize: 512, peakCount: 20)
         
-        if let file = try? AKAudioFile(readFileName: "bassClipCR.wav") {
-            player = AKPlayer(audioFile: file)
-            player.completionHandler = { Swift.print("completion callback has been triggered!") }
-            player.isLooping = true
-            player.buffering = .always
-            
-            loFilter = AKMoogLadder(player)
+        outputLabel.sizeToFit()
+        callousLable.sizeToFit()
+        edgeLabel.sizeToFit()
+        strideLabel.sizeToFit()
+        rumpLabel.sizeToFit()
+        
+          loFilter = AKMoogLadder(input)
             
             //    //MARK: - Eq settings
             //    /***************************************************************/
             //
-            var filterBand2 = AKEqualizerFilter(player, centerFrequency: 35, bandwidth: 44.7, gain: 0.0)
+            filterBand2 = AKEqualizerFilter(loFilter, centerFrequency: 35, bandwidth: 44.7, gain: 0.0)
             filterBand3 = AKEqualizerFilter(filterBand2, centerFrequency: 65, bandwidth: 70.8, gain: 0.0)
             let filterBand4 = AKEqualizerFilter(filterBand3, centerFrequency: 125, bandwidth: 141, gain: 2.0)
             let filterBand5 = AKEqualizerFilter(filterBand4, centerFrequency: 250, bandwidth: 282, gain: 0.022)
@@ -68,7 +73,7 @@ class ViewController: UIViewController {
             //    /***************************************************************/
             //
             
-            var callousness = AKDistortion(player, linearTerm: 0.5, squaredTerm: 2.0, cubicTerm: 2.5, softClipGain: 3.5, finalMix: 0.5)
+            var callousness = AKDistortion(input, linearTerm: 1.0, squaredTerm: 1.0, cubicTerm: 1.0, softClipGain: 3.0, finalMix: 0.5)
             
             
             distMixer = AKDryWetMixer(filterBand7, callousness, balance: 0.0)
@@ -76,7 +81,9 @@ class ViewController: UIViewController {
             //    //MARK: - Output
             //    /***************************************************************/
             //
+        
             booster = AKBooster(distMixer)
+        
             booster?.gain = 0.0
             
             AudioKit.output = booster
@@ -84,63 +91,38 @@ class ViewController: UIViewController {
             try! AudioKit.start()
         }
         
-    }
+
     
-    //    //MARK: - Button and slider methods
+    //    //MARK: - Slider methods
     //    /***************************************************************/
     //
-    @IBAction func startButtonPressed(_ sender: Any) {
-        player.play()
-    }
-    
-    @IBAction func stopButtonPressed(_ sender: Any) {
-        player.stop()
-    }
-    
-    
+   
     @IBAction func cfSliderMoved(_ sender: UISlider) {
         
         var loCutOffVal = filterBand2?.gain = Double(sender.value)
-        print("loCutoffVal \(sender.value)")
+       
     }
     
     @IBAction func resSliderMoved(_ sender: UISlider) {
         
         let loResVal = filterBand3?.gain = Double(sender.value)
-        print(sender.value)
     }
     
     @IBAction func outputVolChanged(_ sender: UISlider) {
         let boosterVal =  booster!.gain = Double(sender.value)
-        print("Output \(sender.value)")
     }
     
     
     @IBAction func callousSliderMoved(_ sender: UISlider) {
         let distVal = distMixer!.balance = Double(sender.value / 8)
-        print("Callous \(sender.value)")
+       
     }
     
     
     
     @IBAction func edgeSliderMoved(_ sender: UISlider) {
         let edgeVal = filterBand7?.gain = Double(sender.value * 4)
-        print("Edge value \(sender.value)")
-    }
-    
-    @IBAction func switchToInput(_ sender: UIButton) {
-        
-        if startBtn.isHidden == false {
-            startBtn.isHidden = true
-            stopBtn.isHidden = true
-            filterBand2 = AKEqualizerFilter(input, centerFrequency: 35, bandwidth: 44.7, gain: 0.0)
-            callousness = AKDistortion(input, linearTerm: 0.5, squaredTerm: 2.0, cubicTerm: 2.5, softClipGain: 3.5, finalMix: 0.5)
-        } else {
-            startBtn.isHidden = false
-            stopBtn.isHidden = false
-            filterBand2 = AKEqualizerFilter(player, centerFrequency: 35, bandwidth: 44.7, gain: 0.0)
-            callousness = AKDistortion(player, linearTerm: 0.5, squaredTerm: 2.0, cubicTerm: 2.5, softClipGain: 3.5, finalMix: 0.5)
-        }
+       
     }
     
 }
